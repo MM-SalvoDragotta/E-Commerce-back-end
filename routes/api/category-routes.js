10d673a151dd4c848bd4dee93a48757dfc146ddd
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-29 20:52:50 +1000

    Implement GET Category By ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,9 +19,25 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryByID = await Category.findByPk(req.params.id, {
+      include: [
+        { model: Product }
+      ],
+    });
+    if (!categoryByID) {
+      res.status(404).json(
+        { message: `No Category with id ${req.params.id} found!` }
+      );
+      return;
+    }
+    res.status(200).json(categoryByID);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
