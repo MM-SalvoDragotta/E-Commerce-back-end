4cda94a25d4988a066c96cb800df60aadbe0f98d
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-30 00:04:32 +1000

    Implement UPDATE Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,8 +50,27 @@ router.post('/', async(req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const update = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {id: req.params.id, }        
+      }
+    );
+    if (!update[0]) {
+      res.status(400).json(
+        { message: `No Category with id ${req.params.id} found!`}
+      );
+      return;
+    }
+    res.status(200).json(update);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
